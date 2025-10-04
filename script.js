353099fa056fed3542be11b2cfea0f409198fea7
Ashutosh Patil <ashutoshpatil470@gmail.com>
2025-10-04 20:22:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
